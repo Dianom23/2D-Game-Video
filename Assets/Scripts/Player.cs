36a0edfab780bd1a4f4f1fab43e72c6fd691f915
Dmitry <dmitry.sh23@gmail.com>
2023-09-29 17:25:32 +0300

    Video 5

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,17 +10,30 @@ public class Player : MonoBehaviour
     private SpriteRenderer _sr;
     [SerializeField] private float _jumpForce = 5;
     [SerializeField] private bool _isGrounded;
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (_moveInput != 0 && _isGrounded)
+        {
+            _animator.SetBool("Move", true);
+        }
+        else
+        {
+            _animator.SetBool("Move", false);
+        }
+
         _rb.velocity = new Vector2(_speed * _moveInput, _rb.velocity.y);
         if (_rb.velocity.x > 0)
         {
